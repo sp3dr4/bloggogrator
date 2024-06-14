91c4d517d539e9813ccbf24b4d8a0f5502ea3594
sp3dr4 <pedra89@gmail.com>
2024-06-14 18:42:53 +0200

    add function to read rss

---
+++ internal/rss/reader.go
@@ -0,0 +1,51 @@
+package rss
+
+import (
+	"encoding/xml"
+	"io"
+	"net/http"
+)
+
+type RSS struct {
+	XMLName xml.Name `xml:"rss"`
+	Channel Channel  `xml:"channel"`
+}
+
+type Channel struct {
+	Title       string `xml:"title"`
+	Description string `xml:"description"`
+	Link        string `xml:"link"`
+	Items       []Item `xml:"item"`
+}
+
+type Item struct {
+	Title       string `xml:"title"`
+	Description string `xml:"description"`
+	Link        string `xml:"link"`
+	PubDate     string `xml:"pubDate"`
+}
+
+func getRssBody(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	return io.ReadAll(resp.Body)
+}
+
+func ReadRss(url string) (*RSS, error) {
+	body, err := getRssBody(url)
+	if err != nil {
+		return nil, err
+	}
+
+	var rss RSS
+	err = xml.Unmarshal(body, &rss)
+	if err != nil {
+		return nil, err
+	}
+
+	return &rss, nil
+}
